2a10688413cd78271d6e738551b6762f5ac65e83
Bartek <bmroczkowski@icloud.com>
2024-04-29 12:05:25 +0200

    Prepare urls for: auth, user.

--- backend/api/views/auth.py
+++ backend/api/views/auth.py
@@ -1,1 +1,20 @@
-urlpatterns = []
+from django.urls import path
+
+
+def auth_login(request) -> None:
+    return
+
+
+def auth_logout(request) -> None:
+    return
+
+
+def auth_register(request) -> None:
+    return
+
+
+urlpatterns = [
+    path("login/", auth_login),
+    path("logout/", auth_logout),
+    path("register/", auth_register),
+]
--- backend/api/views/user.py
+++ backend/api/views/user.py
@@ -1,1 +1,25 @@
-urlpatterns = []
+from django.urls import path
+
+
+def get_me(request) -> None:
+    return
+
+
+def edit_user(request) -> None:
+    return
+
+
+def change_password(request) -> None:
+    return
+
+
+def delete_user(request) -> None:
+    return
+
+
+urlpatterns = [
+    path("me/", get_me),
+    path("edit/", edit_user),
+    path("password/", change_password),
+    path("delete/", delete_user),
+]
