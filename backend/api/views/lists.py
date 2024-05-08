424fd6e8f89df8818217125f16b8f8d602de6ece
Bartek <bmroczkowski@icloud.com>
2024-05-08 12:12:42 +0200

    Add API delete todo list, completed items.

--- backend/api/views/lists.py
+++ backend/api/views/lists.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from django.views.decorators.http import require_http_methods
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+
+from api.models import TodoList, Item
 
 
 @login_required
@@ -18,13 +22,19 @@ def edit_list(request, list_id: int) -> None:
 @login_required
 @require_http_methods(["DELETE"])
 def delete_completed(request, list_id: int) -> None:
-    return
+    todo_list = get_object_or_404(TodoList, id=list_id, author=request.user)
+    Item.objects.filter(todo_list=todo_list, ticked=True).delete()
+
+    return HttpResponse(status=200)
 
 
 @login_required
 @require_http_methods(["DELETE"])
 def delete_list(request, list_id: int) -> None:
-    return
+    todo_list = get_object_or_404(TodoList, id=list_id, author=request.user)
+    todo_list.delete()
+
+    return HttpResponse(status=200)
 
 
 urlpatterns = [
