0d5d8e8a870a4aae9e24b59a7eb86844ce647078
Bartek <bmroczkowski@icloud.com>
2024-07-04 18:21:58 +0200

    Create login form UI.

--- frontend/src/pages/auth/login.tsx
+++ frontend/src/pages/auth/login.tsx
@@ -1,5 +1,24 @@
 export function Login() {
     return (
-        <h1>Login Page</h1>
+        <>
+        <div className="card position-absolute top-50 start-50 translate-middle">
+            <form className="card-body" style={{ width: "450px" }}>
+                <h2 className="text-center mb-3">Login</h2>
+                <div className="mb-3">
+                    <label className="form-label">Email</label>
+                    <input type="email" className="form-control" />
+                </div>
+                <div className="mb-4">
+                    <label className="form-label">Password</label>
+                    <input type="password" className="form-control" />
+                    <div className="form-text">Don't have an account. Create one <a href="/register">here</a>.</div>
+                </div>
+                <button type="submit" className="btn btn-primary w-100">Submit</button>
+            </form>
+        </div>
+        <footer className="position-absolute bottom-0 start-50 translate-middle-x">
+            <p className="text-center text-body-secondary mt-3">© 2024 Todo List</p>
+        </footer>
+        </>
     )
 }
